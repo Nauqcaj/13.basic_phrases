87447d783800ef845534379cf76ad64e294f98f9
Jacquan McClinton <nauqcaj@gmail.com>
2017-10-18 14:19:07 -0500

    Initial commit

---
+++ app/src/main/java/com/jacquan/a13basic_phrases/MainActivity.java
@@ -0,0 +1,35 @@
+package com.jacquan.a13basic_phrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void buttonTapped (View view){
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw","com.jacquan.a13basic_phrases");
+
+        MediaPlayer mPlayer = MediaPlayer.create(this, resourceId);
+        mPlayer.start();
+
+
+        Log.i("button tapped", ourId);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
